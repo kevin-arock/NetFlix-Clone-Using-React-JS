7a54bd4853ec946ac776b92b3bb2121124242f35
kevin-arock <kevinstandlee2002@gmail.com>
2024-09-14 20:16:31 +0530

    setup Firebase

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDKzKKuTNSBbonja8fvH0UcDECEW2AWXts",
+  authDomain: "webflix-7cb68.firebaseapp.com",
+  projectId: "webflix-7cb68",
+  storageBucket: "webflix-7cb68.appspot.com",
+  messagingSenderId: "604272349334",
+  appId: "1:604272349334:web:a156429f15844e81f5b2b7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
